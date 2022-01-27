be1bfad778e3522a7a73cd62071063db5d09dc59
Facundo López <facundo.lopez@decidesoluciones.es>
2022-01-27 15:30:52 +0100

    JavaFX navegación entre ventanas

--- desktop/StoreManagment/src/com/fjl/desktop/storemanagment/controller/InitDataController.java
+++ desktop/StoreManagment/src/com/fjl/desktop/storemanagment/controller/InitDataController.java
@@ -5,18 +5,26 @@
  */
 package com.fjl.desktop.storemanagment.controller;
 
+import java.io.IOException;
 import java.io.StringReader;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import com.fjl.desktop.storemanagment.StoreManagment;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Paint;
 import com.fjl.desktop.storemanagment.util.Init;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import static java.lang.Thread.sleep;
 
@@ -34,6 +42,11 @@ public class InitDataController implements Initializable {
     private ProgressBar progBar;
     @FXML
     private Label progTxt;
+    @FXML
+    private MenuItem aboutButton;
+    @FXML
+    private MenuItem closeButtom;
+
 
     /**
      * Initializes the controller class.
@@ -44,12 +57,48 @@ public class InitDataController implements Initializable {
         progBar.setVisible(false);
         progTxt.setVisible(false);
     }    
-    
-    
 
     @FXML
-    private void clck(ActionEvent event) {
+    private void close(ActionEvent event){
+
+        //System.out.println(node.getId());
+        // Desde un parametro ya inicializado del Nodo pricipal (en este caso el Pane)
+        // Obtengo la ecena (Pane) y luego la ventana  (State)
+        // Para lluego cerrarla
+        Stage st = (Stage) this.btnAuto.getScene().getWindow();
+        st.close();
     }
+
+    @FXML
+    private void click(ActionEvent event){
+        Stage stPadre = (Stage) this.btnAuto.getScene().getWindow();
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            URL url = StoreManagment.class.getResource("view/AboutView.fxml");
+            loader.setLocation(url);
+            InitDataController controlador = loader.getController();
+            //  Cargo el RootNode al
+            Pane aboutWindow = (Pane) loader.load();
+            // Crear Scene con root node.
+            Scene scene = new Scene(aboutWindow);
+            Stage st = new Stage();
+
+            //st.initModality(Modality.APPLICATION_MODAL);
+            st.setScene(scene);
+            st.setOnCloseRequest(event1 -> stPadre.show());
+            st.show();
+
+
+
+            stPadre.hide();
+            //st.setOnCloseRequest(event1 -> controlador.closeWindows());
+        }catch (IOException e){
+            System.out.println(e.getStackTrace());
+        }
+
+
+    }
+    
 
     @FXML
     private void autoGeneret(ActionEvent event) {
